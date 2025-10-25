57f9a34f7400164b1112c7265052fb17fdc5122f
Stoppa Matteo <stoppamatteo@gmail.com>
2025-10-25 15:02:28 +0200

    aggiunto load_env per caricamento variabili di ambiente da file env + SO

--- index.py
+++ index.py
@@ -8,16 +8,12 @@ from utils2.index2 import funzione1, funzione2
 
 # Import externals
 from loguru import logger # per stampa log
+from dotenv import load_dotenv
 
 ## Routine main
 def main():    
-    # info presenti nello script di avvio (visibili solo in windows con il "set" all'interno dello script di npm)
-    #logger.info(`pnpm run script -> ENV: ${process.env.ENV}`)
-    #logger.info(`pnpm run script -> ENV2: ${process.env.ENV2}`)
-
-    # Variabili comuni nella config del package.json
-    #logger.info(`pnpm config -> VAR_A: ${process.env.npm_package_config_VAR_A}`);
-
+    load_dotenv()
+    
     # Variabili presenti nel file .env
     logger.info(f'file env -> FILE_ENV_1: {os.getenv('FILE_ENV_1')}')
     
@@ -28,9 +24,6 @@ def main():
     # Variabili presenti nel Dockerfile
     logger.info(f'docker env -> ENV_DOCKER: {os.getenv('ENV_DOCKER')}')
     logger.info(f'docker env -> TZ: {os.getenv('ENV_DOCKER')}')
-
-    # commento
-    
     
     # time.sleep(30) # per test con container
     
