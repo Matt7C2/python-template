e11a758a3b2b010999b4260c934b73af7b8beb7d
Stoppa Matteo <stoppamatteo@gmail.com>
2025-10-25 22:49:08 +0200

    aggiunto exit per terminazione processo con zero errori

--- index.py
+++ index.py
@@ -23,8 +23,11 @@ def main():
     
     # Variabili presenti nel Dockerfile
     logger.info(f'docker env -> ENV_DOCKER: {os.getenv('ENV_DOCKER')}')
-    logger.info(f'docker env -> TZ: {os.getenv('ENV_DOCKER')}')
+    logger.info(f'docker env -> TZ: {os.getenv('TZ')}')
     
     # time.sleep(30) # per test con container
+
+    # (opzionale) Termina il programma senza errori
+    exit(0)
     
 main()
